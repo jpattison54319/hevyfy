3c827a77031684e8b2e61df4724b351f49468af3
James Pattison <jpattison54319@gmail.com>
2025-07-02 00:44:40 -0400

    fixing data leak

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -9,13 +9,13 @@ import { Auth, browserLocalPersistence, getAuth, GoogleAuthProvider, setPersiste
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBSPzfe-fIq_T5YiH_K3CGFjkXuVtXh_s4",
-  authDomain: "xpetshealth.quest",//"workoutrpg-14ea8.firebaseapp.com",
-  projectId: "workoutrpg-14ea8",
-  storageBucket: "workoutrpg-14ea8.firebasestorage.app",
-  messagingSenderId: "365189784249",
-  appId: "1:365189784249:web:711c00340542c7b238f168",
-  measurementId: "G-YMB052NVCF"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
